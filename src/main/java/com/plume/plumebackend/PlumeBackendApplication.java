4dbc3acd22158f0afea18315ddd820c0bdc205fb
gram <gksmf4721@naver.com>
2024-03-17 19:58:22 +0900

    커밋 테스트

---
+++ src/main/java/com/plume/plumebackend/PlumeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.plume.plumebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plume/plumebackend/PlumeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plume.plumebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
