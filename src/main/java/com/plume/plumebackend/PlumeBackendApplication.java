d3fc64d61c4c8a3ae39548c229ec7d8ed65c0057
njw0921 <n7532056@>
2024-03-19 15:48:44 +0900

    feat: 1. 빌드 시 프로젝트 명 출력 (banana 의존성 추가) 2. application.yml로 확장자 변경, 포트 8080 설정

--- src/main/java/com/plume/plumebackend/PlumeBackendApplication.java
+++ src/main/java/com/plume/plumebackend/PlumeBackendApplication.java
@@ -1,13 +1,26 @@
 package com.plume.plumebackend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
-public class PlumeBackendApplication {
+public class PlumeBackendApplication implements CommandLineRunner {
 
+	private final ConfigurableApplicationContext context;
+
+	public PlumeBackendApplication(ConfigurableApplicationContext context) {this.context = context;}
 	public static void main(String[] args) {
 		SpringApplication.run(PlumeBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		String project = context.getEnvironment().getProperty("application.name");
+		System.out.println(io.leego.banana.BananaUtils.bananaify(project));
+	}
 }
